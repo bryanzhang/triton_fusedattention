42213f11cdddc893b51ee5a6b7506bdfef68e845
bryanzhang <johnhall@126.com>
2024-06-29 17:58:58 +0800

    Compare performance with flashattention_v2's.

--- fused-attention.py
+++ fused-attention.py
@@ -184,13 +184,8 @@ class _attention(torch.autograd.Function):
         return o
 attention = _attention.apply
 
-#try:
-#    from flash_attn.flash_attn_interface import \
-#        flash_attn_qkvpacked_func as flash_attn_func
-#    HAS_FLASH = True
-#except BaseException:
-#    HAS_FLASH = False
-HAS_FLASH = False
+import flash_attn
+from flash_attn import flash_attn_func
 
 BATCH, N_HEADS, HEAD_DIM = 4, 32, 64
 # vary seq length for fixed head and batch=4
@@ -200,10 +195,8 @@ configs.append(
                 x_names=["N_CTX"],
                 x_vals=[2**i for i in range(10, 15)],
                 line_arg="provider",
-                line_vals=["triton-fp16"] +
-                (["flash"] if HAS_FLASH else []),
-                line_names=["Triton [FP16]"] +
-                (["Flash-2"] if HAS_FLASH else []),
+                line_vals=["triton-fp16", "flash-v2"],
+                line_names=["Triton [FP16]", "Flash-v2 [FP16]"],
                 styles=[("red", "-"), ("blue", "-")],
                 ylabel="ms",
                 plot_name=f"fused-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}",
@@ -221,20 +214,41 @@ def bench_flash_attention(BATCH, H, N_CTX, HEAD_DIM, provider, device="cuda"):
     rep = 100
     dtype = torch.float16
     if "triton" in provider:
-        q = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
-        k = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
-        v = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
+        q = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=False)
+        k = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=False)
+        v = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=False)
         sm_scale = 1.3
         fn = lambda: attention(q, k, v, sm_scale)
         ms = triton.testing.do_bench(fn, warmup=warmup, rep=rep)
-    if provider == "flash":
-        qkv = torch.randn((BATCH, N_CTX, 3, H, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
-        fn = lambda: flash_attn_func(qkv)
+    if provider == "flash-v2":
+        q = torch.randn((BATCH, N_CTX, H, HEAD_DIM), dtype=dtype, device=device, requires_grad=False)
+        k = torch.randn((BATCH, N_CTX, H, HEAD_DIM), dtype=dtype, device=device, requires_grad=False)
+        v = torch.randn((BATCH, N_CTX, H, HEAD_DIM), dtype=dtype, device=device, requires_grad=False)
+        fn = lambda: flash_attn_func(q=q, k=k, v=v, dropout_p=float(0.0), softmax_scale=1.3, causal=True, window_size=(-1,-1), alibi_slopes=None, deterministic=False)
         ms = triton.testing.do_bench(fn, warmup=warmup, rep=rep)
     flops_per_matmul = 2.0 * BATCH * H * N_CTX * N_CTX * HEAD_DIM
     total_flops = 2 * flops_per_matmul
     total_flops *= 0.5
     return total_flops / ms * 1e-9
+"""
+    if flash_attention:
+          query_states = self.q_proj(hidden_states)
+          key_states = key_states.transpose(1, 2)
+          value_states = value_states.transpose(1, 2)
+          if flash_attn.__version__ >= "2.0":
+            query_states = query_states.view(bsz, tgt_len, self.num_heads, self.head_dim)
+            attn_output = flash_attn_func(query_states, key_states, value_states, self.dropout, self.scaling, True)
+          else:
+            query_states = query_states.view(bsz * tgt_len, self.num_heads, self.head_dim)
+            key_states = key_states.view(bsz * tgt_len, self.num_heads, self.head_dim)
+            value_states = value_states.view(bsz * tgt_len, self.num_heads, self.head_dim)
+            qkv = torch.stack([query_states, key_states, value_states], dim=1)
+            cu_seqlens = torch.arange(0, (bsz + 1) * tgt_len, tgt_len, dtype=torch.int32, device=query_states.device)
+            attn_output = flash_attn_func(qkv, cu_seqlens, self.dropout, tgt_len, self.scaling, True, False)
+          attn_output = attn_output.reshape(bsz, tgt_len, self.embed_dim)
+          attn_output = self.out_proj(attn_output)
+          return attn_output, None, past_key_value
+"""
 
 # 标准的softmax计算，直接以e为底计算幂函数, 更准确.
 def torch_attention_standard(Q, K, V, sm_scale):
